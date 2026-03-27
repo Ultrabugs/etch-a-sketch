41cef7ba7b1dfef0dc962fcd8ce7c637ba9c5e3d
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-03-27 20:09:31 +0000

    Added lots of features
    Added random number function and added removing function
    Added a ton of new stuff.

--- main.js
+++ main.js
@@ -1,13 +1,60 @@
 
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 
-const div = document.createElement("div");
+const DEFAULT_GRID_SIZE = 16;
 
-let size = 16; 
-
-for(let i = 0; i <= 16; i++) {
-    container.appendChild(div);
-    for(let x = 0; x <= 16; x++){
-        container.appendChild(div);
-    }
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number);
 }
+
+function createGrid(gridSize) {
+    
+    for(let i = 0; i < gridSize; i++) {
+        const divElement = document.createElement("div");
+        divElement.classList.add("div-row");
+        container.appendChild(divElement);
+        
+        for(let x = 0; x < gridSize; x++){
+            const divSubElement = document.createElement("div");
+            divSubElement.classList.add("div-column");
+            divElement.appendChild(divSubElement);
+
+        }
+    }
+    const divSelect = document.querySelectorAll("div.div-column");
+
+    divSelect.forEach((button) => {
+        button.addEventListener("mouseover", () => {
+
+            let red = getRandomNumber(255);
+            let green = getRandomNumber(255);
+            let blue = getRandomNumber(255);
+            let opacityValue = 0.5;
+            button.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            button.style.opacity = opacityValue;
+            
+        });
+    });
+}
+
+function clearGrid() {
+    const divElement = document.querySelectorAll(".div-row"); 
+    divElement.forEach((div) => {
+        div.remove();
+    });
+}
+
+createGrid(DEFAULT_GRID_SIZE);
+
+const btn = document.querySelector("#change-grid-button");
+
+btn.addEventListener("click", () => {
+    let newSize = prompt("Number of sqaures per side for grid");
+    if(newSize >= 100 ) {
+        alert("Maximum size of 100");
+        return;
+    }
+    clearGrid();
+    createGrid(newSize);
+});
+
